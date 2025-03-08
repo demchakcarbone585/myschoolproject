c3d9c7851982c87b680359bdd836a622070a9a09
demchakcarbone585 <demchakcarbone585@gmail.com>
2025-03-08 13:45:12 +0000

    Add 3.js

---
+++ src/3.js
@@ -0,0 +1,17 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+function getRandomGreeting() {
+  var greetings = ["Hello", "Hey", "Hi", "Salut"];
+  return greetings[Math.floor(Math.random() * greetings.length)];
+}
