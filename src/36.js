d03aea621ebcac6fd8a5513b745117d8fc75ee5d
demchakcarbone585 <demchakcarbone585@gmail.com>
2025-05-17 07:37:03 +0000

    Add 36.js

---
+++ src/36.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Educational Project</title>
+</head>
+<body>
+    <div id="main-container"></div>
+
+    <script type="text/javascript" src="myjs.js"></script>
+</body>
+</html>
